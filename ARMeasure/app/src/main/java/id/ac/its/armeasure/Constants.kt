8a196911506ef439a827c225471db080dc4480f0
Kenji Hikmatullah <iniemailkenji@gmail.com>
2020-05-10 00:40:37 +0700

    first

---
+++ ARMeasure/app/src/main/java/id/ac/its/armeasure/Constants.kt
@@ -0,0 +1,8 @@
+package id.ac.its.armeasure
+
+object Constants{
+    const val maxNumMultiplePoints = 6
+    const val multipleDistanceTableHeight = 300
+
+    const val arrowViewSize = 45
+}
--- app/src/main/java/com/shibuiwilliam/arcoremeasurement/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.shibuiwilliam.arcoremeasurement
-
-object Constants{
-    const val maxNumMultiplePoints = 6
-    const val multipleDistanceTableHeight = 300
-
-    const val arrowViewSize = 45
-}
